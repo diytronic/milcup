007756f1f0f2f4285949e24d69c2c2abae0d5968
Roman Tataurov <roman.tataurov@yandex.ru>
2020-03-30 22:42:49 +0300

    Upload boot code done

--- src/com.rs
+++ src/com.rs
@@ -16,6 +16,8 @@ use serialport::{
     // SerialPortType,
 };
 
+// use hex::HexFile;
+
 #[derive(Debug)]
 pub enum FlashError {
     Io(io::Error),
@@ -49,21 +51,71 @@ pub fn set_baud_rate(port: &mut Box<dyn SerialPort>, baud_rate: u32) -> Result<(
     return Ok(());
 }
 
-pub fn read_baud_rate(port: &mut Box<dyn SerialPort>) -> Result<(), FlashError> {
+pub fn read_baud_rate(port: &mut Box<dyn SerialPort>) -> Result<Vec<u8>, FlashError> {
     write_buf(port, vec![0xD])?;
-    let res = read_buf(port, 3)?;
+    let resp = read_buf(port, 3)?;
 
-    println!("{:0>2X?}", res);
-    println!("{:0>2X?}", vec!(0xD, 0xA, 0x3E));
+    // println!("{:0>2X?}", res);
+    // println!("{:0>2X?}", vec!(0xD, 0xA, 0x3E));
 
-    if res != vec!(0xD, 0xA, 0x3E) {
+    if resp != vec!(0xD, 0xA, 0x3E) {
         return Err(FlashError::Io(Error::new(ErrorKind::Other, "Error setting baud rate")));
     }
 
-    // println!("Read {:?}", buf);
+    return Ok(resp);
+}
 
+// dwadrboot: address
+// ilboot: size
+pub fn boot_load(port: &mut Box<dyn SerialPort>, data: crate::hex::HexFile) -> Result<(), FlashError> {
+    println!("Writing boot code to {:0>8X?}", data.addr);
+    println!("Data size is {} bytes", data.size);
+
+    write_buf(port, vec!('L' as u8))?;
+    write_buf(port, data.addr.to_le_bytes().to_vec())?; // address to load code to 
+    write_buf(port, data.size.to_le_bytes().to_vec())?;    // size of data
+
+    if read_buf(port, 1)? != "L".as_bytes() {
+        return Err(FlashError::Io(Error::new(ErrorKind::Other, "Error setting baud rate")));
+    }
+
+    // write boot loader code 1986_BOOT_UART.hex
+    write_buf(port, data.buf)?;
+    if read_buf(port, 1)? != "K".as_bytes() {
+        return Err(FlashError::Io(Error::new(ErrorKind::Other, "Error writing boot code")));
+    }
+    
+    // read and compare
+    // TODO: read and check throught all the data
+    write_buf(port, vec!('Y' as u8))?;
+    write_buf(port, data.addr.to_le_bytes().to_vec())?;  // address to load code to 
+    write_buf(port, (0x8 as u32).to_le_bytes().to_vec())?; // not sure what is it
+
+    let resp = read_buf(port, 10)?; 
+    if resp[0] != ('Y' as u8) && resp[9] != ('K' as u8) {
+        return Err(FlashError::Io(Error::new(ErrorKind::Other, "Error reading written code")));
+    }
+
+    // run code
+    write_buf(port, vec!('R' as u8))?;
+    write_buf(port, data.addr.to_le_bytes().to_vec())?; // address to load code to 
+    write_buf(port, data.size.to_le_bytes().to_vec())?;    // size of data
+
+    if read_buf(port, 1)? != "R".as_bytes() {
+        return Err(FlashError::Io(Error::new(ErrorKind::Other, "Error running boot code")));
+    }
+    
     return Ok(());
 }
+
+pub fn read_info(port: &mut Box<dyn SerialPort>) -> Result<String, FlashError> {
+    write_buf(port, vec!('I' as u8));
+    let res = read_buf(port, 12)?;
+
+    return Ok(String::from_utf8_lossy(&res).to_string());
+}
+
+// ------------------------
 
 fn write_buf(port: &mut Box<dyn SerialPort>, buf: Vec<u8>) -> Result<(), FlashError> {
     println!("Write buf: {:0>2X?} {}", buf, String::from_utf8_lossy(&buf));
@@ -80,14 +132,3 @@ fn read_buf(port: &mut Box<dyn SerialPort>, len: usize) -> Result<Vec<u8>, Flash
     return Ok(buf);
 }
 
-pub fn boot_load(port: &mut Box<dyn SerialPort>) -> Result<(), FlashError> {
-    write_buf(port, vec!('L' as u8));
-    return Ok(());
-}
-
-pub fn read_info(port: &mut Box<dyn SerialPort>) -> Result<(), FlashError> {
-    write_buf(port, vec!('I' as u8));
-    let res = read_buf(port, 12)?;
-
-    return Ok(());
-}
